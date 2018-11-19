a489c71aa566144bfab9e2c31869afed4265ca36
Josh Samuel O. Monreal <mojs@erni.ph>
2018-11-19 22:05:21 +0800

    Removed ERNI.Software.Net.Attributes library

--- UI Layer/FA.UI.Console/Enumerations.cs
+++ UI Layer/FA.UI.Console/Enumerations.cs
@@ -1,27 +1,14 @@
-﻿using ERNI.Software.Net.Attributes;
-
-namespace FaceDetection
+﻿namespace FaceDetection
 {
     public class Enumerations
     {
         public enum APIOption
         {
-            [Title("Person Group Creation")]
             PersonGroupCreation = 1,
-
-            [Title("Person Group Training")]
             PersonGroupTraining = 2,
-
-            [Title("Person Creation")]
             PersonCreation = 3,
-
-            [Title("Person Face Creation")]
             PersonFaceCreation = 4,
-
-            [Title("Face Detection")]
             FaceDetection = 5,
-
-            [Title("Face Identification")]
             FaceIdentification = 6,
         }
     }
