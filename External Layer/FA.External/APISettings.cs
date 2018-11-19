15a8bfdbb9210b27b9f1455f7d45fa8a7b6d8113
Josh Samuel O. Monreal <mojs@erni.ph>
2018-11-19 22:04:49 +0800

    Changed Subscription Key

--- External Layer/FA.External/APISettings.cs
+++ External Layer/FA.External/APISettings.cs
@@ -2,7 +2,7 @@
 {
     public static class APISettings
     {
-        public const string SUBSCRIPTION_KEY = "e5c8ca7b80654c899cb24b61c2858514";
+        public const string SUBSCRIPTION_KEY = "19708299df154bcb858138d5381fc49b";
         public const string URI_BASE = "https://southeastasia.api.cognitive.microsoft.com/face/v1.0/";
     }
 }
