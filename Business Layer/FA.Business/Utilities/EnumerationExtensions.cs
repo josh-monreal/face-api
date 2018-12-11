881731fd2616636d305508191a107389565d4574
Josh Samuel O. Monreal <mojs@erni.ph>
2018-12-11 16:48:43 +0800

    Created an EnumerationExtension Class
    This static class has a GetDescription<T> method which will extract the value of the Description Attribute in a specific enumeration.

---
+++ Business Layer/FA.Business/Utilities/EnumerationExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+
+namespace FA.Business.Utilities
+{
+    public static class EnumerationExtensions
+    {
+        public static string GetDescription<T>(this T enumeration) where T : IConvertible
+        {
+            if (enumeration is Enum)
+            {
+                Type type = enumeration.GetType();
+                Array values = Enum.GetValues(type);
+
+                foreach (int value in values)
+                {
+                    if (value == enumeration.ToInt32(CultureInfo.InvariantCulture))
+                    {
+                        var memberInfos = type.GetMember(type.GetEnumName(value));
+
+                        if (memberInfos[0].GetCustomAttributes(typeof(DescriptionAttribute), false)
+                            .FirstOrDefault() is DescriptionAttribute descriptionAttribute)
+                        {
+                            return descriptionAttribute.Description;
+                        }
+                    }
+                }
+            }
+
+            return null;
+        }
+    }
+}
