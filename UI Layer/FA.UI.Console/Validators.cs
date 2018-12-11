2b66ff80a371654a28ea880d1f670fa170eef220
Josh Samuel O. Monreal <mojs@erni.ph>
2018-12-11 12:16:54 +0800

    Created an Instance of the Validators class
    For each method, I created an instance of the Validators class since I have changed it from being a static class.

--- UI Layer/FA.UI.Console/Validators.cs
+++ UI Layer/FA.UI.Console/Validators.cs
@@ -4,42 +4,61 @@ using System.Text.RegularExpressions;
 
 namespace FaceDetection
 {
-    public static class Validators
+    public class Validators
     {
-        public static void PersonGroup(string personGroupId)
+        public bool IsValid { get; private set; }
+
+        public Validators()
+        {
+            IsValid = false;
+        }
+
+        public void PersonGroup(string personGroupId)
         {
             if (!new Regex(@"^[a-z0-9_-]{1,64}$").IsMatch(personGroupId))
             {
                 string error = "The person group ID that you intend to create contains invalid characters.";
+                IsValid = false;
                 throw new InvalidOperationException(error);
-            };
+            }
+            else
+                IsValid = true;
         }
 
-        public static void PersonName(string name)
+        public void PersonName(string name)
         {
             if (!new Regex(@".{1,128}").IsMatch(name))
             {
                 string error = "The person's name is too long.";
+                IsValid = false;
                 throw new InvalidOperationException(error);
             }
+            else
+                IsValid = true;
         }
 
-        public static void PersonId(string id)
+        public void PersonId(string id)
         {
-            if(!new Regex(@"^[a-z0-9]{8}[-][a-z0-9]{4}[-][a-z0-9]{4}[-][a-z0-9]{4}[-][a-z0-9]{12}$").IsMatch(id))
+            if (!new Regex(@"^[a-z0-9]{8}[-][a-z0-9]{4}[-][a-z0-9]{4}[-][a-z0-9]{4}[-][a-z0-9]{12}$").IsMatch(id))
             {
                 string error = "The person's ID is invalid.";
+                IsValid = false;
                 throw new InvalidOperationException(error);
             }
+            else
+                IsValid = true;
         }
 
-        public static void ImagePath(string filePath)
+        public void ImagePath(string filePath)
         {
             if (!File.Exists(filePath))
             {
                 string error = "The image that you provided does not exist.";
+                IsValid = false;
                 throw new InvalidOperationException(error);
             }
+            else
+                IsValid = true;
         }
     }
 }
