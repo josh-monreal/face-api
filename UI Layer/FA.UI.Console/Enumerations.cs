124e19e5c5f57f3c0fb5ec6931ea204e192fb457
Josh Samuel O. Monreal <mojs@erni.ph>
2018-12-11 16:47:03 +0800

    Added a Description Attribute
    Used the Description Attribute of System.ComponentModel for the enumerations under the UI and Business layers.

--- UI Layer/FA.UI.Console/Enumerations.cs
+++ UI Layer/FA.UI.Console/Enumerations.cs
@@ -1,14 +1,27 @@
-﻿namespace FaceDetection
+﻿using System.ComponentModel;
+
+namespace FaceDetection
 {
     public class Enumerations
     {
         public enum APIOption
         {
+            [Description("Create a Person Group")]
             PersonGroupCreation = 1,
+
+            [Description("Train a Person Group")]
             PersonGroupTraining = 2,
+
+            [Description("Create a Person")]
             PersonCreation = 3,
+
+            [Description("Create a Face For a Person")]
             PersonFaceCreation = 4,
+
+            [Description("Detect a Face")]
             FaceDetection = 5,
+
+            [Description("Identify a Face")]
             FaceIdentification = 6,
         }
     }
