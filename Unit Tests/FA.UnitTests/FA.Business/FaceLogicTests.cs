0131c3ed7e1e8a257d0b569124df4d16a1481669
Josh Samuel O. Monreal <mojs@erni.ph>
2019-01-02 13:49:38 +0800

    Created Unit Tests for FaceLogic
    Further details can be found at https://github.com/josh-monreal/face-api/issues/11.

--- Unit Tests/FA.UnitTests/FA.Business/FaceLogicTests.cs
+++ Unit Tests/FA.UnitTests/FA.Business/FaceLogicTests.cs
@@ -5,6 +5,7 @@ using FA.Business.Models;
 using FA.External.Core;
 using Moq;
 using NUnit.Framework;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -30,12 +31,86 @@ namespace FA.UnitTests.FA.Business
         public void DetectFace_InvalidImageFormat_ResponseDataMustBeNull()
         {
             _faceAPI.Setup(api => api.Detect("a"))
-                .Returns(Task.FromResult(new HttpResponseMessage { StatusCode = HttpStatusCode.NotFound }));
+                .Returns(Task.FromResult(new HttpResponseMessage { StatusCode = HttpStatusCode.BadRequest }));
 
-            _responseHelper.Setup(rh => rh.CreateResponse<DetectedFaceDto>(
+            _responseHelper.Setup(rh => rh.CreateResponse<DetectedFaceDto[]>(
                     It.Is<HttpResponseMessage>(res => !res.IsSuccessStatusCode),
                     It.IsAny<string>()))
                 .Returns(new Response { Data = null });
+
+            _faceLogic = new FaceLogic(_faceAPI.Object, _responseHelper.Object);
+
+            var result = _faceLogic.DetectFace("a");
+
+            Assert.That(result.Data, Is.Null);
+        }
+
+        [Test]
+        public void DetectFace_ValidImageFormat_ResponseDataMustBeAnObject()
+        {
+            _faceAPI.Setup(api => api.Detect("a"))
+                .Returns(Task.FromResult(new HttpResponseMessage { StatusCode = HttpStatusCode.OK }));
+
+            _responseHelper.Setup(rh => rh.CreateResponse<DetectedFaceDto[]>(
+                    It.Is<HttpResponseMessage>(res => res.IsSuccessStatusCode),
+                    It.IsAny<string>()))
+                .Returns(new Response { Data = new DetectedFaceDto[] { new DetectedFaceDto { FaceId = "abc" }}});
+
+            _faceLogic = new FaceLogic(_faceAPI.Object, _responseHelper.Object);
+
+            var result = _faceLogic.DetectFace("a");
+
+            Assert.That(((DetectedFaceDto[])result.Data)[0].FaceId, Is.EqualTo("abc"));
+        }
+
+        [Test]
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase(" ")]
+        public void DetectFace_ImagePathIsNullOrEmpty_ThrowArgumentNullException(string error)
+        {
+            _faceLogic = new FaceLogic(_faceAPI.Object, _responseHelper.Object);
+
+            Assert.That(() => _faceLogic.DetectFace(error), Throws.ArgumentNullException);
+        }
+
+        [Test]
+        [TestCase(HttpStatusCode.OK)]
+        [TestCase(HttpStatusCode.BadRequest)]
+        public void IdentifyFace_WhenCalled_ReturnAString(HttpStatusCode httpStatusCode)
+        {
+            _faceAPI.Setup(api => api.Identify(new List<string> { "a" }, "a"))
+                .Returns(Task.FromResult(new HttpResponseMessage { StatusCode = httpStatusCode, Content = new StringContent("") }));
+
+            _responseHelper.Setup(rh => rh.JsonPrettyPrint(
+                    It.IsAny<HttpResponseMessage>(),
+                    It.IsAny<string>()))
+                .Returns(string.Empty);
+
+            _faceLogic = new FaceLogic(_faceAPI.Object, _responseHelper.Object);
+
+            var result = _faceLogic.IdentifyFace(new List<string> { "a" }, "a");
+
+            Assert.That(result, Is.TypeOf<string>());
+        }
+
+        [Test]
+        public void IdentifyFace_FaceIDIsNull_ThrowArgumentNullException()
+        {
+            _faceLogic = new FaceLogic(_faceAPI.Object, _responseHelper.Object);
+
+            Assert.That(() => _faceLogic.IdentifyFace(null, "a"), Throws.ArgumentNullException);
+        }
+
+        [Test]
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase(" ")]
+        public void IdentifyFace_PersonGroupIsNullOrEmpty_ThrowArgumentNullException(string error)
+        {
+            _faceLogic = new FaceLogic(_faceAPI.Object, _responseHelper.Object);
+
+            Assert.That(() => _faceLogic.IdentifyFace(new List<string>(), error), Throws.ArgumentNullException);
         }
     }
 }
