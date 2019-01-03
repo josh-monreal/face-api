fb34fb4630d3fc64e938e7820f3715af6d566b4f
Josh Samuel O. Monreal <mojs@erni.ph>
2019-01-03 14:46:25 +0800

    Created an IHttpHelper interface for dependency injection

---
+++ External Layer/FA.External/Core/IHttpHelper.cs
@@ -0,0 +1,11 @@
+﻿using System.Net.Http;
+
+namespace FA.External.Core
+{
+    public interface IHttpHelper
+    {
+        HttpClient GetHttpClient();
+        HttpContent CreateHttpContent(object obj, string contentType);
+        ByteArrayContent CreateByteArrayContent(string imagePath, string contentType);
+    }
+}
