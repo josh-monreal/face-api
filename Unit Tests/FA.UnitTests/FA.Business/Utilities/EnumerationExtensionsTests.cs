d9ee67e27d2b7ace32006f7a03b754c7f5e987d7
Josh Samuel O. Monreal <mojs@erni.ph>
2019-01-03 08:40:09 +0800

    Created Unit Tests For EnumerationExtensions
    More details can be found at https://github.com/josh-monreal/face-api/issues/26.

---
+++ Unit Tests/FA.UnitTests/FA.Business/Utilities/EnumerationExtensionsTests.cs
@@ -0,0 +1,42 @@
+﻿using FA.Business.Utilities;
+using NUnit.Framework;
+using scm = System.ComponentModel;
+
+namespace FA.UnitTests.FA.Business.Utilities
+{
+    public enum SampleEnumeration
+    {
+        [scm.Description("One")]
+        One = 1,
+
+        [scm.Description("Two")]
+        Two = 2,
+
+        [scm.Description("Three")]
+        Three = 3
+    }
+
+    [TestFixture]
+    public class EnumerationExtensionsTests
+    {
+        [Test]
+        public void GetDescription_IfTypeIsEnum_ReturnDescription()
+        {
+            var enumeration = SampleEnumeration.One;
+
+            var result = enumeration.GetDescription();
+
+            Assert.That(result, Is.EqualTo("One"));
+        }
+
+        [Test]
+        public void GetDescription_IfTypeIsNotEnum_ReturnNull()
+        {
+            var str = string.Empty;
+
+            var result = str.GetDescription();
+
+            Assert.That(result, Is.Null);
+        }
+    }
+}
