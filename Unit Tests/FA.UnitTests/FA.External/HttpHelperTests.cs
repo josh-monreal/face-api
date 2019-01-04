9197ddecc864bcf5e3dec6d324993ed4956b451f
Josh Samuel O. Monreal <mojs@erni.ph>
2019-01-04 09:07:58 +0800

    Created Unit Tests For the HttpHelper class

---
+++ Unit Tests/FA.UnitTests/FA.External/HttpHelperTests.cs
@@ -0,0 +1,42 @@
+﻿using FA.External;
+using NUnit.Framework;
+
+namespace FA.UnitTests.FA.External
+{
+    [TestFixture]
+    public class HttpHelperTests
+    {
+        private HttpHelper _helper;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _helper = new HttpHelper();
+        }
+
+        [Test]
+        public void GetHttpClient_WhenCalled_ShouldReturnHttpClientWithAPIKey()
+        {
+            var result = _helper.GetHttpClient();
+
+            Assert.That(result.DefaultRequestHeaders.Contains("Ocp-Apim-Subscription-Key"), Is.True);
+        }
+
+        [Test]
+        public void CreateHttpContent_WhenCalled_ShouldReturnHttpContentWithItem()
+        {
+            var item = new
+            {
+                Id = 1,
+                Name = "a"
+            };
+
+            var result = _helper.CreateHttpContent(item, "application/json");
+
+            Assert.That(
+                result.ReadAsStringAsync()
+                    .Result, 
+                Contains.Substring("Id"));
+        }
+    }
+}
