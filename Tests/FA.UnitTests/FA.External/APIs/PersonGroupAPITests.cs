63fed8a3f8d58df3e19c7749395c82ac709fff6e
Josh Samuel O. Monreal <mojs@erni.ph>
2019-01-04 12:25:17 +0800

    Moved the FA.UnitTests Project to the Tests Folder

---
+++ Tests/FA.UnitTests/FA.External/APIs/PersonGroupAPITests.cs
@@ -0,0 +1,154 @@
+﻿using FA.External;
+using FA.External.APIs;
+using FA.External.Core;
+using Moq;
+using Moq.Protected;
+using NUnit.Framework;
+using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FA.UnitTests.FA.External.APIs
+{
+    [TestFixture]
+    public class PersonGroupAPITests
+    {
+        private PersonGroupAPIs _api;
+        private Mock<IHttpHelper> _helper;
+        private Mock<HttpMessageHandler> _messageHandler;
+
+        private Uri _uri;
+
+        private const int TIMES_CALLED = 1;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _messageHandler = new Mock<HttpMessageHandler>(MockBehavior.Strict);
+            _messageHandler
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>(
+                    "SendAsync",
+                    ItExpr.IsAny<HttpRequestMessage>(),
+                    ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(new HttpResponseMessage())
+                .Verifiable();
+
+            _helper = new Mock<IHttpHelper>();
+            _helper.Setup(hlpr => hlpr.GetHttpClient())
+                .Returns(new HttpClient(_messageHandler.Object));
+        }
+
+        [Test]
+        public void Create_WhenCalled_CreatePutRequest()
+        {
+            var item = new { Id = 1, Name = "a" };
+            _helper.Setup(hlpr => hlpr.CreateHttpContent(item, "a"))
+                 .Returns(new StringContent("a"));
+
+            _api = new PersonGroupAPIs(_helper.Object);
+
+            var result = _api.Create("a");
+
+            _messageHandler
+                .Protected()
+                .Verify(
+                    "SendAsync",
+                    Times.Exactly(TIMES_CALLED),
+                    ItExpr.IsAny<HttpRequestMessage>(),
+                    ItExpr.IsAny<CancellationToken>());
+        }
+
+        [Test]
+        public void Create_WhenCalled_RequestUriMustBeEqualToExpectedUri()
+        {
+            _uri = new Uri(APISettings.URI_BASE + "persongroups/a");
+
+            var item = new { Id = 1, Name = "a" };
+            _helper.Setup(hlpr => hlpr.CreateHttpContent(item, "a"))
+                 .Returns(new StringContent("a"));
+
+            _api = new PersonGroupAPIs(_helper.Object);
+
+            var result = _api.Create("a");
+
+            _messageHandler
+               .Protected()
+               .Verify(
+                   "SendAsync",
+                   Times.Exactly(TIMES_CALLED),
+                   ItExpr.Is<HttpRequestMessage>(req => req.RequestUri == _uri),
+                   ItExpr.IsAny<CancellationToken>());
+        }
+
+        [Test]
+        public void Create_WhenCalled_RequestMethodShouldBePut()
+        {
+            var item = new { Id = 1, Name = "a" };
+            _helper.Setup(hlpr => hlpr.CreateHttpContent(item, "a"))
+                .Returns(new StringContent("a"));
+
+            _api = new PersonGroupAPIs(_helper.Object);
+
+            var result = _api.Create("a");
+
+            _messageHandler
+                .Protected()
+                .Verify(
+                    "SendAsync",
+                    Times.Exactly(TIMES_CALLED),
+                    ItExpr.Is<HttpRequestMessage>(req => req.Method == HttpMethod.Put),
+                    ItExpr.IsAny<CancellationToken>());
+        }
+
+        [Test]
+        public void Train_WhenCalled_CreatePostRequest()
+        {
+            _api = new PersonGroupAPIs(_helper.Object);
+
+            var result = _api.Train("a");
+
+            _messageHandler
+                .Protected()
+                .Verify(
+                    "SendAsync",
+                    Times.Exactly(TIMES_CALLED),
+                    ItExpr.IsAny<HttpRequestMessage>(),
+                    ItExpr.IsAny<CancellationToken>());
+        }
+
+        [Test]
+        public void Train_WhenCalled_RequestUriMustBeEqualToExpectedUri()
+        {
+            _uri = new Uri(APISettings.URI_BASE + "persongroups/a/train");
+            _api = new PersonGroupAPIs(_helper.Object);
+
+            var result = _api.Train("a");
+
+            _messageHandler
+               .Protected()
+               .Verify(
+                   "SendAsync",
+                   Times.Exactly(TIMES_CALLED),
+                   ItExpr.Is<HttpRequestMessage>(req => req.RequestUri == _uri),
+                   ItExpr.IsAny<CancellationToken>());
+        }
+
+        [Test]
+        public void Train_WhenCalled_RequestMethodShouldBePost()
+        {
+            _api = new PersonGroupAPIs(_helper.Object);
+
+            var result = _api.Train("a");
+
+            _messageHandler
+                .Protected()
+                .Verify(
+                    "SendAsync",
+                    Times.Exactly(TIMES_CALLED),
+                    ItExpr.Is<HttpRequestMessage>(req => req.Method == HttpMethod.Post),
+                    ItExpr.IsAny<CancellationToken>());
+        }
+    }
+}
