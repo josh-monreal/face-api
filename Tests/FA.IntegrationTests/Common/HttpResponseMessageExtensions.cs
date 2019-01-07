7eaaead5d837793d0ee9ef9d30c1c4927b24dd58
Josh Samuel O. Monreal <mojs@erni.ph>
2019-01-07 16:00:21 +0800

    Created ParameterConstants and HttpResponseMessageExtensions classes

---
+++ Tests/FA.IntegrationTests/Common/HttpResponseMessageExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace FA.IntegrationTests.Common
+{
+    public static class HttpResponseMessageExtensions
+    {
+        public static string GetStringContent(this HttpResponseMessage response)
+        {
+            return response
+                .Content
+                .ReadAsStringAsync()
+                .Result;
+        }
+
+        public static MediaTypeHeaderValue GetContentType(this HttpResponseMessage response)
+        {
+            return response
+                .Content
+                .Headers
+                .ContentType;
+        }
+    }
+}
