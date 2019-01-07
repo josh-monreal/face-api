680b64b79bf4f48f6bfbf1925ce527c044ac97c8
Josh Samuel O. Monreal <mojs@erni.ph>
2019-01-07 15:51:14 +0800

    Renamed the APISettings.cs file to APIConstants.cs

--- External Layer/FA.External/APISettings.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace FA.External
-{
-    public static class APISettings
-    {
-        public const string SUBSCRIPTION_KEY = "bf7ba2514c4c4d528b2ec9be9d52c69f";
-        public const string URI_BASE = "https://southeastasia.api.cognitive.microsoft.com/face/v1.0/";
-    }
-}
