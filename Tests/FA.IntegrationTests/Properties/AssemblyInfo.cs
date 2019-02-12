d2c0c54ffbb1ec870e6921f6e3526157803c98d1
Josh Samuel O. Monreal <mojs@erni.ph>
2019-02-12 14:10:50 +0800

    Modified the AssemblyInfo information of each project/class library
    More details can be found at https://github.com/josh-monreal/face-api/issues/63.

--- Tests/FA.IntegrationTests/Properties/AssemblyInfo.cs
+++ Tests/FA.IntegrationTests/Properties/AssemblyInfo.cs
@@ -3,18 +3,14 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("FA.IntegrationTests")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Proof-of-concept application that uses Face API of Azure Cognitive Services")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FA.IntegrationTests")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyProduct("FaceAPI")]
+[assembly: AssemblyCopyright("Copyright © 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
 [assembly: Guid("ef095173-e783-4176-8470-3d40ada3eb96")]
-
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
