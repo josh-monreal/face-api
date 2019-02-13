41262d6f84e7509ef017b56c8f8b36fd473eee37
Josh Samuel O. Monreal <mojs@erni.ph>
2019-02-13 15:53:22 +0800

    Created a PersonValidator class
    More details can be found at https://github.com/josh-monreal/face-api/issues/61.

---
+++ Business Layer/FA.Business/Validators/PersonValidator.cs
@@ -0,0 +1,13 @@
+﻿using FA.Business.DTOs;
+using FluentValidation;
+
+namespace FA.Business.Validators
+{
+    public class PersonValidator : AbstractValidator<PersonDto>
+    {
+        public PersonValidator()
+        {
+            RuleFor(p => p.Name).Matches(".{1,128}");
+        }
+    }
+}
